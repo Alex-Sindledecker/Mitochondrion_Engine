b2778409a61b1cf0ab66ee4025b95ea62c98d165
Alex Sindledecker <alexsindledecker1@gmail.com>
2020-09-17 18:26:09 -0700

    Removed CRT memory leak detection

--- Core/src/Debug/DebugTools.h
+++ Core/src/Debug/DebugTools.h
@@ -3,15 +3,5 @@
 #define _CRTDBG_MAP_ALLOC
 
 #include <assert.h>
-#include <stdlib.h>
-#include <crtdbg.h>
 
 #define ENGINE_ASSERT(expression) assert(expression)
-
-#ifdef ENGINE_DEV
-	//#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#elif ENGINE_DEBUG
-	//#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#else
-
-#endif
