64137f032a170bb8dc263be56de16be49a787873
Alex Sindledecker <alexsindledecker1@gmail.com>
2020-11-17 22:14:11 -0800

    Project Setup

---
+++ Engine/mepch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//Data structures
+#include <unordered_map>
+#include <array>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+
+//Tools
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <stdlib.h>
+
+//External Resources
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Mitochondrion Engine - *Powerhouse of the game*
-A 3D game engine that is intended to specialize in particle and voxel rendering.
+# Mitochondrion Engine - *The Powerhouse of the Game*
